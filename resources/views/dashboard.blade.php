54710f0445e266a45ca219ece8346882aa425292
gitcahub <chantikaginting01.gmail.com>
2026-07-03 09:50:17 +0700

    tambah link css dashboardpenumpang

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Penumpang - Velozza</title>
+    <link rel="stylesheet" href="{{ asset('css/dashboardpenumpang.css') }}"> 
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50 font-sans">
